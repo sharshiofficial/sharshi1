0c67681a3ba8cf1ede62b29cce4373744a217a4b
sharshi <sharshiofficial@gmail.com>
2025-11-24 01:45:58 +0530

    Add files via upload

---
+++ sharshi_site/src/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-b from-red-700 to-black text-white">
+      <h1>SHASHRSHI</h1>
+    </div>
+  );
+}
---
+++ sharshi_site/src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './Home.jsx'
+import './style.css'
+ReactDOM.createRoot(document.getElementById('root')).render(<Home />)
